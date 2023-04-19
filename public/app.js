2b218dd8d6375f33ae906f0ef49c9da5009c81f5
yfengtech <moonlight920@outlook.com>
2023-04-19 23:06:46 +0800

    psn page

---
+++ public/app.js
@@ -0,0 +1,31 @@
+const API_URL = 'https://api.playstation.com/userProfile/v1/users/{user_id}/trophies?fields=trophyTitleSmallIconUrl,trophyRare,trophyEarnedRate,fromUser&platform=PS4,PS5&npLanguage=en';
+
+const USER_ID = 'showery-mode68';
+
+const TROPHY_CONTAINER = document.getElementById('trophy-container');
+
+function getTrophies() {
+  fetch(API_URL.replace('{user_id}', USER_ID))
+    .then(response => response.json())
+    .then(data => {
+      const trophies = data.trophyTitles.slice(0, 5);
+      TROPHY_CONTAINER.innerHTML = '';
+      trophies.forEach(trophy => {
+        const trophyCard = document.createElement('div');
+        trophyCard.classList.add('trophy');
+        const trophyImage = document.createElement('img');
+        trophyImage.src = trophy.trophyTitleSmallIconUrl;
+        const trophyTitle = document.createElement('h2');
+        trophyTitle.textContent = trophy.name;
+        const trophyDescription = document.createElement('p');
+        trophyDescription.textContent = trophy.trophyEarnedRate + '% earned';
+        trophyCard.appendChild(trophyImage);
+        trophyCard.appendChild(trophyTitle);
+        trophyCard.appendChild(trophyDescription);
+        TROPHY_CONTAINER.appendChild(trophyCard);
+      });
+    })
+    .catch(error => console.error(error));
+}
+
+getTrophies();
